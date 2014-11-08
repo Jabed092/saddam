98ba75d75ba20dc8bc7db9d1d77e97203e8bba69
kkr07 <motu.aneeq@gmail.com>
2014-11-08 10:49:28 +0600

    new commit

---
+++ ManyToManyRelationship/ManyToManyRelationship/EnrollmentUI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManyToManyRelationship
+{
+    public partial class EnrollmentUI : Form
+    {
+
+        public EnrollmentUI(List<Student> students, List<Course> courses, List<Enrollment> enrollments )
+        {
+            InitializeComponent();
+            PopulateStudentComboBox(students);
+        }
+
+        private void PopulateStudentComboBox(List<Student> students)
+        {
+            studentComboBox.DisplayMember = "RegNo";
+            studentComboBox.DataSource = students;
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
